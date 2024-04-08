556df7bf201855a4c1559ae5ab87a98fa8a11174
Daniel <dosmond37@gmail.com>
2024-04-08 16:07:16 -0600

    Fix quotes

--- components/about/AboutMe.tsx
+++ components/about/AboutMe.tsx
@@ -3,8 +3,8 @@ export const AboutMe = () => {
     <section className="flex flex-col items-center justify-center w-full h-[110vh] bg-base-100 bottom-clip">
       <h1 className="text-4xl font-bold text-center">About Me</h1>
       <p className="text-center">
-        I'm a software developer with a passion for creating and building
-        things. I love to learn new things and I'm always looking for new
+        I&apos;m a software developer with a passion for creating and building
+        things. I love to learn new things and I&apos;m always looking for new
         challenges.
       </p>
     </section>
